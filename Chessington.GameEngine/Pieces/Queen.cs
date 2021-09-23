33613229523c86a4d82ea6e24f887f27a9c9ddf8
Josh Batt <J.Batt@ucas.ac.uk>
2021-09-23 12:27:52 +0100

    Test 6 Complete

--- Chessington.GameEngine/Pieces/Queen.cs
+++ Chessington.GameEngine/Pieces/Queen.cs
@@ -13,33 +13,19 @@ namespace Chessington.GameEngine.Pieces
             Square currentPos = board.FindPiece(this);
             List<Square> moves = new List<Square>();
 
-            //Rook Movement
-
-            for (int i = 0; i < 8; i++)
-            {
-                if (currentPos.Col != i)
-                {
-                    moves.Add(Square.At(currentPos.Row, i));
-                }
-                if (currentPos.Row != i)
-                {
-                    moves.Add(Square.At(i, currentPos.Col));
-                }
-                    
-            }
-
-            //Bishop Movement
+            //Bishop and Rook movement combined
 
             for (int x = 0; x < 8; x++)
             {
                 for (int y = 0; y < 8; y++)
                 {
-                    if ((Square.At(x, y).Row - currentPos.Row == Square.At(x, y).Col - currentPos.Col || currentPos.Row - Square.At(x, y).Row == Square.At(x, y).Col - currentPos.Col) && currentPos != Square.At(x, y))
+                    if ((Square.At(x, y).Row - currentPos.Row == Square.At(x, y).Col - currentPos.Col || currentPos.Row - Square.At(x, y).Row == Square.At(x, y).Col - currentPos.Col || Square.At(x, y).Row == currentPos.Row || Square.At(x, y).Col == currentPos.Col) && currentPos != Square.At(x, y ))
                     {
                         moves.Add(Square.At(x, y));
                     }
                 }
             }
+
             return moves;
         }
     }
